ad2342c831ad314a1197334f30ea8f705fc4a381
Suraj Jadhav <jadhavsuraj4567@gmail.com>
2023-12-08 13:03:45 +0530

    getting products on frontend

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,18 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
-
+import { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
 export default function Dashboard({ auth }) {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            let res = await fetch("http://127.0.0.1:8000/api/getproducts");
+            res = await res.json();
+            setData(res);
+        }
+        fetchData();
+    }, []);
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -16,9 +27,37 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
-                        </div>
+                        <Table variant="dark" striped bordered hover size="sm">
+                            <thead>
+                                <tr>
+                                    <th>Sr.No.</th>
+                                    <th>Product Name</th>
+                                    <th>Product Image</th>
+                                    <th>Product Price(in Rs.)</th>
+                                    <th>Product description</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data?.map((item, index) => {
+                                    return (
+                                        <tr key={item.id}>
+                                            <td>{++index}</td>
+                                            <td>{item.name}</td>
+                                            <td>
+                                                <img
+                                                    width={"100px"}
+                                                    height={"100px"}
+                                                    src={`http://127.0.0.1:8000/${item.file_path}`}
+                                                    alt="product image"
+                                                />
+                                            </td>
+                                            <td>{item.price}</td>
+                                            <td>{item.description}</td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </Table>
                     </div>
                 </div>
             </div>
